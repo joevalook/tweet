7eddb03b908629fdc16bf1c211ce3839300264ce
Joe Valookaran <joevalookaran@hotmail.com>
2022-02-02 03:09:20 +0000

    skipped way too many commits again, finished the tweets page, new tweets and moving nav bar

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+  console.log("javascript is loading!");
+  $("textarea").on("input",function(event){
+    counter = 140 - this.value.length;
+    $(this).parent().find("output")[0].innerHTML = counter;
+    if (counter < 0) {
+      $(this).parent().find("output").addClass("redFont")
+    }
+    else if (counter < 10) {
+      $(this).parent().find("output").addClass("orangeFont")
+    }
+  })
+});
