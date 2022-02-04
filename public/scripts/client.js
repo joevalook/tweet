f800b7460b558077605b9d0b29f807d7559726f1
Joe Valookaran <joevalookaran@hotmail.com>
2022-02-04 04:01:11 +0000

    added the comments into the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// added function definitions outside of document.ready so actions in document.ready are clear
+
+// encode the tweets to prevent cross-site scripting
 function htmlEncode(str){
   return String(str).replace(/[^\w. ]/gi, function(c){
      return '&#'+c.charCodeAt(0)+';';
   });
 }
+//create a tweet element from an object taken from API
 const createTweetElement = (tweetData) => {
   const layout = `
     <article class="tweets" id="boxShadow">
@@ -41,14 +46,17 @@ const createTweetElement = (tweetData) => {
   `
   return layout
 }
+
+//add tweet element to end of other tweets
 const appendTweetElement = (tweetData) => {
   $('#tweetBox').append(createTweetElement(tweetData));
 }
-
+//add tweet element before other tweets
 const prependTweetElement = (tweetData) => {
   $('#tweetBox').prepend(createTweetElement(tweetData));
 }
 
+// used to loop through objects in API and created proper tweets in html
 const renderTweets = function(tweets) {
   for (let i = tweets.length-1; i >=0; i--) {
     appendTweetElement(tweets[i])
@@ -56,11 +64,13 @@ const renderTweets = function(tweets) {
 }
 
 $(function() {
+  // get the most recent tweet object
   const loadRecentTweet = function(url) {
     $.get(url).then((data) => {
       prependTweetElement(data.slice(-1)[0]);
   })
   }
+  // render tweets from API
   const loadTweets = function(url) {
     $.get(url).then((data) => {
       renderTweets(data);
@@ -70,15 +80,18 @@ $(function() {
 
   const $form = $('#tweetForm');
   const $textbox = $('#tweet-text')
+  // when there is no longer an error in the tweetbox remove errors from html
   $textbox.on('input', function() {
     if (document.getElementById('tweetForm')[0].value.length <= 140 && document.getElementById('tweetForm')[0].value.length !== 0 ) {
       $(document.getElementById('tweet-text')).removeClass("error");
       $("#error").slideUp("fast")
     }
   })
+
   $form.on('submit', function (e) {
     e.preventDefault();
     console.log();
+    // display errors if tweet is not between 0 and 140 characters
     if(this[0].value.length > 140) {
       $(document.getElementById('tweet-text')).addClass("error");
       document.getElementById('error').innerHTML = "* Error: You can not have over 140 characters!";
@@ -89,20 +102,24 @@ $(function() {
       document.getElementById('error').innerHTML = "* Error: You can not post nothing!";
       $("#error").slideDown("fast")
     }
+    // ajax post tweets
     else {
       $.post("/tweets/", $(this).serialize(),() => {
         loadRecentTweet("/tweets/")
         document.getElementById('counter').innerHTML = '140';
+        // cause tweets to scroll to top, so user can see there tweet 'pop' to the top of the live feed
         $('#tweetBox').animate({ scrollTop: 0 }, 'slow');
       });
+      // make textbox empty again
       $form[0].reset();
     }
   })
+  // create a sliding down new tweet form, so user can choose when to write a tweet
   $('#writeTweet, #angles').on('click', function() {
     if ($('#newTweet').is(":hidden")){  
       $("#newTweet").slideDown("slow")
-      $("tweet-text").focus();
       $('.container').css("height", "74vh")
+      // change arrows to match what direction the new tweet form will go when pressed
       $('#angles').removeClass('fa-angles-down')
       $('#angles').addClass('fa-angles-up')
     }
